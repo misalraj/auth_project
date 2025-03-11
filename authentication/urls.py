12a0ada2fc6602853fa957175cd789ca6456c15b
Misal <misalraj@gmail.com>
2025-03-11 13:42:03 +0530

    Created a view for OTP verification and updated urlPath

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserRegistrationView
+from .views import UserRegistrationView,UserRegistrationVerifyView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
+    path('register/verify/', UserRegistrationVerifyView.as_view(), name='register-verify'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,4 @@
 # Create your views here.
-
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -12,3 +11,8 @@ class UserRegistrationView(APIView):
             serializer.save()
             return Response({"message": "User registered successfully. Please check your email for OTP."}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class UserRegistrationVerifyView(APIView):
+    def post(self, request):
+        # Implement OTP verification logic here
+        return Response({"message": "User verified successfully."}, status=status.HTTP_200_OK)
