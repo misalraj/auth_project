a4be400b94e3f56fb6c61c2829833b2ccc7267d5
Misal <misalraj@gmail.com>
2025-03-11 13:45:54 +0530

    Created a view for user logina and updated urlPath

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserRegistrationView,UserRegistrationVerifyView
+from .views import UserRegistrationView,UserRegistrationVerifyView, UserLoginView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('register/verify/', UserRegistrationVerifyView.as_view(), name='register-verify'),
+    path('login/', UserLoginView.as_view(), name='login'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -3,6 +3,8 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import UserRegistrationSerializer
+from django.contrib.auth import authenticate, login
+
 
 class UserRegistrationView(APIView):
     def post(self, request):
@@ -16,3 +18,15 @@ class UserRegistrationVerifyView(APIView):
     def post(self, request):
         # Implement OTP verification logic here
         return Response({"message": "User verified successfully."}, status=status.HTTP_200_OK)
+    
+class UserLoginView(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        user = authenticate(request, email=email, password=password)
+        if user:
+            login(request, user)
+            response = Response({"message": "Login successful."}, status=status.HTTP_200_OK)
+            response.set_cookie('auth_token', 'your_auth_token', httponly=True, secure=True)
+            return response
+        return Response({"message": "Invalid credentials."}, status=status.HTTP_401_UNAUTHORIZED)
