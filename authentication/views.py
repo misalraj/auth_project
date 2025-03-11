9f618fa9ecd961b5dd6f0f1e5fe26b6409703e24
Misal <misalraj@gmail.com>
2025-03-11 13:23:24 +0530

    Created a view for user registration and updated url path

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
 # Create your views here.
+
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import UserRegistrationSerializer
+
+class UserRegistrationView(APIView):
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "User registered successfully. Please check your email for OTP."}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
